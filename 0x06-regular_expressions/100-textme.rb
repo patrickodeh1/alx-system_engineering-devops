7207943e65ca55bfdab0109af633962974925a4c
Patrick Odeh <vyltyan@gmail.com>
2024-05-10 07:33:05 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -8,7 +8,7 @@ def parse_log_entry(log_entry)
 end
 
 # Read the log file line by line
-File.open('logfile.txt', 'r') do |file|
+File.open('text_messages.txt', 'r') do |file|
     file.each_line do |line|
         # Extract log entry timestamp and content
         timestamp, log_entry = line.match(/^\w{3} \d{1,2} \d{2}:\d{2}:\d{2} (.*?mdr: .*)$/)[1].split(' ', 2)

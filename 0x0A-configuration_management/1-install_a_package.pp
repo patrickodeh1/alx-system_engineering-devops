34374c1e455d0d3f46fa3ff7dbb079021ec53c93
Patrick <vyltyan@gmail.com>
2024-07-03 15:02:44 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# This manifest installs Flask version 2.1.0 using pip3
+# This manifest installs Flask version 2.1.0 and Werkzeug version 2.0.3 using pip3
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
+  require  => Package['flask'],
 }

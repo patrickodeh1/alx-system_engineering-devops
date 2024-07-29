751f776811d87b447827b14f23c36e41597e6746
Patrick Odeh <vyltyan@gmail.com>
2024-07-30 00:56:47 +0100

    corrections"

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -24,29 +24,17 @@ def get_data(employee_id):
         todo_response.raise_for_status()
         todo_data = todo_response.json()
 
-        total_tasks = len(todo_data)
-        completed = [task for task in todo_data if task.get("completed")]
-        num_of_completed = len(completed)
+        filename = "{}.csv".format(employee_id)
+        with open(filename, mode='w', newline='') as csv_file:
+            writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-        print("Employee {} is done with tasks({}/{}):"
-              .format(employee_name, num_of_completed, total_tasks))
-        for task in completed:
-            print("\t {}".format(task.get('title')))
-
-            filename = "{}.csv".format(employee_id)
-            with open(filename, mode='w', newline='') as csv_file:
-                fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                              "TASK_TITLE"]
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-                writer.writeheader()
-                for task in todo_data:
-                    writer.writerow({
-                        "USER_ID": employee_id,
-                        "USERNAME": employee_name,
-                        "TASK_COMPLETED_STATUS": task.get("completed"),
-                        "TASK_TITLE": task.get("title")
-                    })
+            for task in todo_data:
+                writer.writerow({
+                    employee_id,
+                    employee_name,
+                    task.get("completed"),
+                    task.get("title")
+                })
     except requests.exceptions.RequestException as e:
         pass
 

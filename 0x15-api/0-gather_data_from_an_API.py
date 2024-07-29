e9db25d9ac48929dbbbce6b2d1d3260f22570b02
Patrick Odeh <vyltyan@gmail.com>
2024-07-30 00:18:48 +0100

    task 0 and 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" gather data from an API """
+import requests
+import sys
+
+
+def get_data(employee_id):
+    user_url = "https://jsonplaceholder.typicode.com/users/{}\
+        ".format(employee_id)
+    user_response = requests.get(user_url)
+    user_response.raise_for_status()
+    user_data = user_response.json()
+
+    employee_name = user_data.get("name")
+
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    todo_response = requests.get(todo_url)
+    todo_response.raise_for_status()
+    todo_data = todo_response.json()
+
+    total_tasks = len(todo_data)
+    completed = [task for task in todo_data if task.get("completed")]
+    num_of_completed = len(completed)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_of_completed, total_tasks))
+    for task in completed:
+        print("\t {}".format(task.get('title')))
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+
+    if not employee_id.isdigit():
+        sys.exit()
+
+    get_data(int(employee_id))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" gather data from an API """
+import requests
+import sys
+import csv
+
+
+def get_data(employee_id):
+    user_url = "https://jsonplaceholder.typicode.com/users/{}\
+        ".format(employee_id)
+    user_response = requests.get(user_url)
+    user_response.raise_for_status()
+    user_data = user_response.json()
+
+    employee_name = user_data.get("name")
+
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    todo_response = requests.get(todo_url)
+    todo_response.raise_for_status()
+    todo_data = todo_response.json()
+
+    total_tasks = len(todo_data)
+    completed = [task for task in todo_data if task.get("completed")]
+    num_of_completed = len(completed)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_of_completed, total_tasks))
+    for task in completed:
+        print("\t {}".format(task.get('title')))
+
+        filename = "{}.csv".format(employee_id)
+        with open(filename, mode='w', newline='') as csv_file:
+            fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                          "TASK_TITLE"]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for task in todo_data:
+                writer.writerow({
+                    "USER_ID": employee_id
+                    "USERNAME": employee_name
+                    "TASK_COMPLETED_STATUS": task.get("completed"),
+                    "TASK_TITLE": task.get("title")
+                })
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+
+    if not employee_id.isdigit():
+        sys.exit()
+
+    get_data(int(employee_id))
